73fb592ef2f24c8e25a53b13cb7dbd742a668a34
Fırat Şakar <frtskr0@gmail.com>
2022-08-12 15:06:48 +0300

    changed project color class

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class PorjectColors {
+class ProjectColors {
   static const Color firebaseFirstOrange = Color(0xFFFFA611);
   static const Color firebaseSecondOrange = Color(0xFFF6820D);
   static const Color firebaseThirdOrange = Color(0xFFFFCB2B);
+  static const Color colorWhite = Colors.white;
 }
