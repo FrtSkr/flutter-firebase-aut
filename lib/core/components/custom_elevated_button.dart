cb37488d783b09885eda184a7e789deb5a4204a6
Fırat Şakar <frtskr0@gmail.com>
2022-08-12 15:06:25 +0300

    created custom elevated button

---
+++ lib/core/components/custom_elevated_button.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_aut/core/colors/colors.dart';
+import 'package:firebase_aut/core/constants/constant_edge_insets.dart';
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  const CustomElevatedButton({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const ShapeDecoration(
+          shape: StadiumBorder(),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            colors: [
+              ProjectColors.firebaseFirstOrange,
+              ProjectColors.firebaseSecondOrange,
+            ],
+          )),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: Colors.transparent,
+          shadowColor: Colors.transparent,
+          padding: ConstantEdgeInsets.paddingOnlyRL50,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                color: ProjectColors.colorWhite,
+              ),
+        ),
+      ),
+    );
+  }
+}
