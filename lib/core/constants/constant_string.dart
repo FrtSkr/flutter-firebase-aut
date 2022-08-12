78018f8af94de30608007fe40c9308539804bfb0
Fırat Şakar <frtskr0@gmail.com>
2022-08-12 13:40:25 +0300

    changed const string class

--- lib/core/constants/constant_string.dart
+++ lib/core/constants/constant_string.dart
@@ -1,6 +1,6 @@
 class ConstantString {
-  static const String USER_NAME_HINT = "username";
-  static const String PASSWORD_HINT = "password";
+  static const String USER_NAME_HINT = "User Name";
+  static const String PASSWORD_HINT = "Password";
   static const String FORGOT_PASS_BUTTON = "Forgot Password?";
   static const String LOGIN_TITLE = "LOGIN SCREEN";
   static const String SIGN_IN_BUTTON_TITLE = "SIGN IN";
