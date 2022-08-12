038b277dc9faba628186fb40054fb853553a6fbf
Fırat Şakar <frtskr0@gmail.com>
2022-08-12 13:19:55 +0300

    created constant edge insets class

---
+++ lib/core/constants/constant_edge_insets.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ConstantEdgeInsets {
+  static const EdgeInsets paddingAll10 = EdgeInsets.all(10);
+  static const EdgeInsets paddingOnlyRL50 =
+      EdgeInsets.only(right: 50, left: 50);
+  static const EdgeInsets paddingVertical10 =
+      EdgeInsets.symmetric(vertical: 10);
+  static const EdgeInsets paddingHorizontal10 =
+      EdgeInsets.symmetric(horizontal: 10);
+}
