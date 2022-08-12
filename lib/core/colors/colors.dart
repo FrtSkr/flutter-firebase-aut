0c5805ddf20d44ef50b32e21267468f9203141b4
Fırat Şakar <frtskr0@gmail.com>
2022-08-12 12:41:04 +0300

    firebase colors

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/cupertino.dart';
 
+class PorjectColors {
+  static const Color firebaseFirstOrange = Color(0xFFFFA611);
+  static const Color firebaseSecondOrange = Color(0xFFF6820D);
+  static const Color firebaseThirdOrange = Color(0xFFFFCB2B);
+}
