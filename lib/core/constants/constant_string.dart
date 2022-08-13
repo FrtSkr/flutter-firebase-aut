45fd017ac3563d894a16a8dced2edd69f198e991
Fırat Şakar <frtskr0@gmail.com>
2022-08-14 00:34:24 +0300

    added string values

--- lib/core/constants/constant_string.dart
+++ lib/core/constants/constant_string.dart
@@ -1,9 +1,17 @@
 class ConstantString {
-  static const String USER_NAME_HINT = "User Name";
-  static const String PASSWORD_HINT = "Password";
+  static const String USER_ID = "User ID";
+  static const String PASSWORD = "Password";
   static const String FORGOT_PASS_BUTTON = "Forgot Password?";
   static const String HELP_BUTTON = "Help";
   static const String LOGIN_TITLE = "LOGIN SCREEN";
   static const String SIGN_IN_BUTTON_TITLE = "SIGN IN";
   static const String REGISTER_BUTTON_TITLE = "CREATE AN ACCOUNT";
+  static const String PROJECT_NAME = "Firebase Aut. Project";
+  static const String OR_TEXT = "OR";
+  static const String CREATE_ACCOUNT = "Create Account";
+  static const String PHONE_NUMBER = "Phone Number";
+  static const String EMAIL = "Email";
+  static const String LAST_NAME = "Last Name";
+  static const String FIRST_NAME = "First Name";
+  static const String CREATE_ACC = "Create Account";
 }
