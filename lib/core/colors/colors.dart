56b2f3e803a01a1f24d49a6983804ee8a3d5b614
Fırat Şakar <frtskr0@gmail.com>
2022-08-14 00:32:32 +0300

    added colors

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -5,6 +5,7 @@ class ProjectColors {
   static const Color firebaseFirstOrange = Color(0xFFFFA611);
   static const Color firebaseSecondOrange = Color(0xFFF6820D);
   static const Color firebaseThirdOrange = Color(0xFFFFCB2B);
-  static const Color colorWhite = Colors.white;
   static const Color colorGrey400 = Color(0xFFBDBDBD);
+  static const Color colorWhite = Colors.white;
+  static const Color colorBlack54 = Colors.black54;
 }
