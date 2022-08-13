22b771a8dd1236673edf4d91283321774f3bacc6
Fırat Şakar <frtskr0@gmail.com>
2022-08-14 00:33:31 +0300

    changed onPressed

--- lib/core/components/custom_text_button.dart
+++ lib/core/components/custom_text_button.dart
@@ -1,15 +1,28 @@
-import 'package:firebase_aut/core/colors/colors.dart';
+import 'package:firebase_aut/core/navigation/page_navigation.dart';
+import 'package:firebase_aut/features/login/view/login_view.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextButton extends StatelessWidget {
-  const CustomTextButton({Key? key, required this.title, required this.color})
+  const CustomTextButton(
+      {Key? key,
+      required this.title,
+      required this.color,
+      this.context,
+      this.widget})
       : super(key: key);
   final String title;
   final Color color;
+  final BuildContext? context;
+  final Widget? widget;
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
+      onPressed: () {
+        if (this.context != null && widget != null) {
+          PageNavigation.navigateToWidget(
+              this.context ?? context, widget ?? const LoginScreen());
+        }
+      },
       child: Text(
         title,
         style: Theme.of(context).textTheme.subtitle2?.copyWith(
