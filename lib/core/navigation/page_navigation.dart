18523433054da9e0e154652ab1e8aa3cadfe54e5
Fırat Şakar <frtskr0@gmail.com>
2022-08-13 23:51:52 +0300

    created page navigate

---
+++ lib/core/navigation/page_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+mixin PageNavigation {
+  static void navigateToWidget(BuildContext context, Widget widget) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        fullscreenDialog: true,
+        settings: const RouteSettings(),
+      ),
+    );
+  }
+}
