5b098af28a8b28d1909f57d13f2468d16a43f1a2
Fırat Şakar <frtskr0@gmail.com>
2022-08-17 20:13:20 +0300

    created register service

---
+++ lib/features/register/service/register_service.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_database/firebase_database.dart';
+import '../../users/model/user_model.dart';
+
+class RegisterService {
+  final referenceDatabase = FirebaseDatabase.instance;
+  Future createUser(User user) async {
+    final ref = referenceDatabase.ref();
+    final json = user.toJson();
+    print(json);
+    await ref.child("users").push().set(json).asStream();
+  }
+}
